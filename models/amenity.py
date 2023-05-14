288f019c860c7c0879d3392714b4e2cc01abfc65
Soliu <sawlew@gmail.com>
2023-05-14 21:05:52 +0100

    test

--- models/amenity.py
+++ models/amenity.py
@@ -6,3 +6,7 @@ from models.base_model import BaseModel
 class Amenity(BaseModel):
     """Representation of Amenity """
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
